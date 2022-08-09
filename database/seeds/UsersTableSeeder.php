f0efb28fc2c36c63a2a1306063f25877b841e8b1
John Doe <antonioalvarez713@gmail.com>
2022-08-08 22:05:04 -0500

    User Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      User::create([
+        "name" => "Jose",
+        "email" => "jose@mail.com",
+        "password" => Hash::make("123123"),
+      ]);
+
+      User::create([
+        "name" => "Antonio",
+        "email" => "antonio@mail.com",
+        "password" => Hash::make("123123"),
+      ]);
+    }
+}
